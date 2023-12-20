d01a4e23f1c0225b323076697431f6961e96c71d
Maks <maksmunait@gmail.com>
2023-12-20 15:42:26 +0100

    commit main

--- src/main.c
+++ src/main.c
@@ -1,6 +1,130 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <MLV/MLV_all.h>
+#include <time.h>
+#include <math.h>
+#include <string.h>
+
+
+
+
+// Dimensions de la grille en nombre de cases (origine en haut a gauche) :
+// début: (0, 0), fin: (17, 11) 
+#define LIGNES 18
+#define COLONNES 12
+
+
+
+// enum {
+// 	ABEILLE = 'A',
+// 	FRELON = 'F'
+// }
+
+
+// Les deux camps :
+#define ABEILLE 'A'
+#define FRELON 'F'
+
+// Les types d'unites :
+#define REINE 'r'
+#define OUVRIERE 'o'
+#define ESCADRON 'e'
+#define GUERRIERE 'g'
+#define FRELON 'f'
+#define RUCHE 'R'
+#define NID 'N'
+
+
+// Pour la recolte de pollen
+#define RECOLTE 'p'
+
+
+// Les temps necessaires a la production :
+#define TREINEA 8
+#define TREINEF 8
+#define TOUVRIERE 2
+#define TGUERRIERE 4
+#define TESCADRON 6
+#define TFRELON 5
+#define TRECOLTE 4
+
+
+// Les couts necessaires a la production abeilles :
+#define CRUCHE 10
+#define CREINEA 7
+#define COUVRIERE 3
+#define CGUERRIERE 5
+#define CESCADRON 6
+
+// Les couts necessaires a la production frelons :
+#define CNID 10
+#define CREINEF 8
+#define CFRELON 3
+
+
+// La force des unites
+#define FREINE 6
+#define FOUVRIERE 1
+#define FGUERRIERE 5
+#define FESCADRON 12
+#define FFRELON 8
+
+
+
+
+
+// La structure Unite :
+typedef struct unite {
+	char camp; 								// ABEILLE ou FRELON
+	char type; 								// RUCHE, NID, REINE, OUVRIERE, GUERRIERE, ESCADRON ou FRELON
+	int force; 								// la force de l'unite
+	int posx, posy; 						// position actuelle sur la grille
+	int destx, desty; 						// destination (negatif si immobile)
+	char production; 						// production d'une ruche ou d'un nid et RECOLTE pour la recolte de pollen
+	int temps; 								// nombres de tours total pour cette production
+	int toursrestant; 						// tours restant pour cette production
+	struct unite *usuiv, *uprec;			// liste des unites affiliees a une ruche ou un nid
+	struct unite *colsuiv, *colprec;		// liste des autres ruches ou nids (colonies) du même camp
+	struct unite *vsuiv, *vprec;			// liste des autres unites sur la meme case
+} Unite, *UListe;
+
+
+// La structure Case :
+typedef struct {
+	Unite *colonie; 		// S'il y a une ruche ou un nid sur la case
+	UListe occupant; 		// les autres occupants de la case
+} Case;
+
+
+// La structure Grille :
+typedef struct {
+	Case plateau[LIGNES][COLONNES];
+	UListe abeille, frelon;
+	int tour; 				// Numero du tour
+	int ressourcesAbeille, ressourcesFrelon;
+} Grille;
+
+
+
+
+void free_abeilles(){
+	fprintf(stderr, "Abeilles sont détruites! \n");
+}
+
+void free_frelons(){
+	fprintf(stderr, "Frelons sont détruites! \n");
+}
+
+
+void initialisation_abeille(void){
+	// il y a une ruche, une reine, une ouvrière et une guerrière 
+	
+	if (NULL == (Unite *))
+}
+
+
 
 int main(int argc, char *argv[]){
-  
-  return 0;
+	
+    return 0;
 }
