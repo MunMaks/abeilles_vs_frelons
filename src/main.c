f84a683e66f50e51ab27767fad7695aaad342152
Maks Munait <the.splatt.gg@gmail.com>
2023-12-26 00:10:49 +0100

    commit useless

--- src/main.c
+++ src/main.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <MLV/MLV_all.h>
-#include <time.h>
-#include <math.h>
-#include <string.h>
+//#include <MLV/MLV_all.h>
+//#include <time.h>
+//#include <math.h>
+//#include <string.h>
 
 
 
