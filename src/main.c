3fd1d52284dbe8e64d4966a6c9a98b5aab4201dd
Maks Munait <the.splatt.gg@gmail.com>
2023-12-26 18:29:45 +0100

    first commit

--- src/main.c
+++ src/main.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
-//#include <MLV/MLV_all.h>
-//#include <time.h>
-//#include <math.h>
-//#include <string.h>
-
-
+// #include <MLV/MLV_all.h>
+#include <time.h>
+// #include <math.h>
+#include <string.h>
 
 
 // Dimensions de la grille en nombre de cases (origine en haut a gauche) :
@@ -13,17 +11,9 @@
 #define LIGNES 18
 #define COLONNES 12
 
-
-
-// enum {
-// 	ABEILLE = 'A',
-// 	FRELON = 'F'
-// }
-
-
 // Les deux camps :
-#define ABEILLE 'A'
-#define FRELON 'F'
+#define ABEILLES 'A'
+#define FRELONS 'F'
 
 // Les types d'unites :
 #define REINE 'r'
@@ -34,10 +24,8 @@
 #define RUCHE 'R'
 #define NID 'N'
 
-
 // Pour la recolte de pollen
 #define RECOLTE 'p'
-
 
 // Les temps necessaires a la production abeilles :
 #define TREINEA 8
@@ -52,7 +40,6 @@
 #define COUVRIERE 3
 #define CGUERRIERE 5
 #define CESCADRON 6
-
 
 
 // Les temps necessaires a la production frelons :
@@ -73,11 +60,9 @@
 #define FFRELON 8
 
 
-
-
 // La structure Unite :
 typedef struct unite {
-	char camp; 								// ABEILLE ou FRELON
+	char camp; 								// ABEILLES ou FRELONS
 	char type; 								// RUCHE, NID, REINE, OUVRIERE, GUERRIERE, ESCADRON ou FRELON
 	int force; 								// la force de l'unite
 	int posx, posy; 						// position actuelle sur la grille
@@ -109,27 +94,181 @@ typedef struct {
 
 
 
-void free_abeilles(){
+void free_abeilles(void){
 	fprintf(stderr, "Abeilles sont détruites! \n");
 }
 
-void free_frelons(){
+void free_frelons(void){
 	fprintf(stderr, "Frelons sont détruites! \n");
 }
 
 
-void initialisation_abeille(void){
-	// il y a une ruche, une reine, une ouvrière et une guerrière 
-	Unite * lst = NULL;
+// Il faut retravailler ici
+Grille *initialiserGrille(void) {
+    // Allocate memory for the Grille structure
+    Grille *grille = (Grille *)malloc(sizeof(Grille));
 
-	if (NULL == (lst = (Unite *) malloc(Unite)) ){
-		fprintf(stderr, "Pas de mémoire\n");
+    if (grille == NULL) {
+        fprintf(stderr, "Malloc n'a pas reussi a allouer la memoire, reessayez.\n");
+        return NULL; 	// exit(EXIT_FAILURE);
+    }
+
+    grille->tour = 1;
+
+    grille->ressourcesAbeille = 0;		// par defaut 0
+    grille->ressourcesFrelon = 0;		// par defaut 0
+
+ 
+    for (int i = 0; i < LIGNES; ++i) {
+        for (int j = 0; j < COLONNES; ++j) {
+			// Chaque case de plateau est initialisee par defaut NULL
+            grille->plateau[i][j].colonie = NULL;	
+            grille->plateau[i][j].occupant = NULL;
+        }
+    }
+	
+    grille->abeille = NULL;
+    grille->frelon = NULL;
+
+	grille->plateau[0][0].colonie = grille->abeille;
+    grille->plateau[LIGNES - 1][COLONNES - 1].colonie = grille->frelon;
+
+
+    return grille;
+}
+
+Unite * initialisation_ruche(void){
+	Unite * A_Ruche = NULL;
+    if (NULL == (A_Ruche = (Unite *)malloc(sizeof(Unite)))) {
+        fprintf(stderr, "Pas de mémoire pour Abeille Ruche\n");
+		return NULL;
 	}
+	A_Ruche->camp = ABEILLES;
+	A_Ruche->type = RUCHE;
+	A_Ruche->force = 0;
+	A_Ruche->posx = 0, A_Ruche->posy = 0;
+	A_Ruche->destx = -1, A_Ruche->desty = -1;
+	A_Ruche->production = -1;
+	A_Ruche->temps = -1;
+	A_Ruche->toursrestant = 1; 			// QUESTION
+	A_Ruche->usuiv = NULL, A_Ruche->uprec = NULL;
+	A_Ruche->colsuiv = NULL, A_Ruche->colprec = NULL;
+	A_Ruche->vsuiv = NULL, A_Ruche->vprec = NULL;
+
+	return A_Ruche;
+}
+
+
+Unite *initialisation_reine(void) {
+    Unite *A_Reine = NULL;
+    if (NULL == (A_Reine = (Unite *)malloc(sizeof(Unite)))) {
+        fprintf(stderr, "Pas de mémoire pour Abeille Reine\n");
+        return NULL;
+    }
+
+    A_Reine->camp = ABEILLES;
+    A_Reine->type = REINE;
+    A_Reine->force = 0;
+    A_Reine->posx = 0, A_Reine->posy = 0;
+    A_Reine->destx = -1, A_Reine->desty = -1;
+    A_Reine->production = -1;
+    A_Reine->temps = -1;
+    A_Reine->toursrestant = 1;
+    A_Reine->usuiv = NULL, A_Reine->uprec = NULL;
+    A_Reine->colsuiv = NULL, A_Reine->colprec = NULL;
+    A_Reine->vsuiv = NULL, A_Reine->vprec = NULL;
+
+    return A_Reine;
+}
+
+Unite *initialisation_ouvriere(void) {
+    Unite *A_Ouvriere = NULL;
+    if (NULL == (A_Ouvriere = (Unite *)malloc(sizeof(Unite)))) {
+        fprintf(stderr, "Pas de mémoire pour Abeille Ouvriere\n");
+        return NULL;
+    }
+
+    A_Ouvriere->camp = ABEILLES;
+    A_Ouvriere->type = OUVRIERE;
+    A_Ouvriere->force = 0;
+    A_Ouvriere->posx = 0, A_Ouvriere->posy = 0;
+    A_Ouvriere->destx = -1, A_Ouvriere->desty = -1;
+    A_Ouvriere->production = -1;
+    A_Ouvriere->temps = -1;
+    A_Ouvriere->toursrestant = 1;
+    A_Ouvriere->usuiv = NULL, A_Ouvriere->uprec = NULL;
+    A_Ouvriere->colsuiv = NULL, A_Ouvriere->colprec = NULL;
+    A_Ouvriere->vsuiv = NULL, A_Ouvriere->vprec = NULL;
+
+    return A_Ouvriere;
+}
+
+Unite *initialisation_guerriere(void) {
+    Unite *A_Guerriere = NULL;
+    if (NULL == (A_Guerriere = (Unite *)malloc(sizeof(Unite)))) {
+        fprintf(stderr, "Pas de mémoire pour Abeille Guerriere\n");
+        return NULL;
+    }
+
+    A_Guerriere->camp = ABEILLES;
+    A_Guerriere->type = GUERRIERE;
+    A_Guerriere->force = 0;
+    A_Guerriere->posx = 0, A_Guerriere->posy = 0;
+    A_Guerriere->destx = -1, A_Guerriere->desty = -1;
+    A_Guerriere->production = -1;
+    A_Guerriere->temps = -1;
+    A_Guerriere->toursrestant = 1;
+    A_Guerriere->usuiv = NULL, A_Guerriere->uprec = NULL;
+    A_Guerriere->colsuiv = NULL, A_Guerriere->colprec = NULL;
+    A_Guerriere->vsuiv = NULL, A_Guerriere->vprec = NULL;
+
+    return A_Guerriere;
 }
 
 
 
+
+
+
+
+UListe initialisation_abeilles(void){
+	// Camp ABEILLE au debut: 1 ruche, 1 reine, 1 guerrière et 1 ouvrière
+	Unite *A_Ruche = initialisation_ruche();
+	Unite *A_Reine = initialisation_reine();
+	Unite *A_Ouvriere = initialisation_ouvriere();
+	Unite *A_Guerriere = initialisation_guerriere();
+
+	A_Ruche->usuiv = A_Reine;
+    A_Reine->uprec = NULL;
+
+    A_Reine->usuiv = A_Guerriere;
+    A_Reine->uprec = A_Ruche;
+
+    A_Guerriere->usuiv = A_Ouvriere;
+    A_Guerriere->uprec = A_Reine;
+
+    A_Ouvriere->usuiv = NULL;
+    A_Ouvriere->uprec = A_Guerriere;
+
+	return A_Ruche;
+}
+
+
+
+void libererPlateau(Grille *grille) {
+    free(grille);
+}
+
+
+
+
 int main(int argc, char *argv[]){
+	Grille *grille = initialiserGrille();
+
 	
+	libererPlateau(grille);
+
+	printf("Memoire est liberee !\n");
+
     return 0;
 }
